43eee56cb44dd97b37fe381dc72308c15bab74bd
Allen <tkmzor@gmail.com>
2026-01-03 19:29:11 +0200

    fix: netlify MIME and asset loading issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/quiz-app/',
+  base: mode === 'production' ? '/quiz-app/' : '/',
   server: {
     allowedHosts: [
       'allow-all',
     ],
   },
-})
+}))
